b2c45d6a1bf68296f5316bb3c7689ef956dc3600
mfkfawas <mfkfawas@gmail.com>
2025-03-21 13:47:19 +0300

    chore: setup ShadCN UI for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
   return (
     <>
-      <h1 className="bg-red-500">test</h1>
+      <Button className="bg-red-400">Shadcn test</Button>
     </>
   );
 }
