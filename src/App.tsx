e11b7884f96d926eb0642b9ee857aa86aba6877d
mfkfawas <mfkfawas@gmail.com>
2025-03-22 14:10:48 +0300

    feat: setup axios with json-server for API requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { Button } from '@/components/ui/button';
 import { ModeToggle } from '@/components/mode.toggle';
+import axios from 'axios';
+
+const fetchUsers = async () => {
+  const res = await axios.post('http://localhost:4002/tasks', {
+    text: 'jiberlekka a book',
+    completed: true,
+    deleted: false,
+    createdAt: '2025-02-03T14:00:00Z',
+  });
+  console.log('🚀 ~ fetchUsers ~ res:', res);
+};
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Button className="bg-red-500">Shadcn dark mode</Button>
+      <Button onClick={fetchUsers} className="bg-red-500">
+        Shadcn dark mode
+      </Button>
       <ModeToggle />
     </ThemeProvider>
   );
